7b1ef86a5b8291c67557ede762eebc146b851475
GlauberRoque <80328841+GlauberRoque@users.noreply.github.com>
2022-01-05 20:11:48 -0300

    Initial

---
+++ ConsumirApi/Controllers/HomeController.cs
@@ -0,0 +1,111 @@
+﻿using ConsumirApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace ConsumirApi.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<ActionResult> Pessoas()
+        {
+            string BaseUrl = "http://localhost:5043/";
+
+            List<Pessoa>? pessoas = new List<Pessoa>();
+
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(BaseUrl);
+            client.DefaultRequestHeaders.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+            HttpResponseMessage response = await client.GetAsync("api/pessoas");
+            if (response.IsSuccessStatusCode)
+            {
+                var dados = response.Content.ReadAsStringAsync().Result;
+                pessoas = JsonConvert.DeserializeObject<List<Pessoa>>(dados);
+            }
+            return View(pessoas);
+
+        }
+
+       public async Task<ActionResult> PessoaId(int id)
+        {
+            string BaseUrl = "http://localhost:5043/";
+            Pessoa? p = new Pessoa();
+
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(BaseUrl);
+            client.DefaultRequestHeaders.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+            HttpResponseMessage response = await client.GetAsync("api/pessoas/" + id);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var dados = response.Content.ReadAsStringAsync().Result;
+                p = JsonConvert.DeserializeObject<Pessoa>(dados);
+            }
+            return View(p);
+
+        }
+
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Cadastrar(Pessoa p)
+        {
+            string BaseUrl = "http://localhost:5043/";
+
+            HttpClient client = new HttpClient();
+
+            HttpResponseMessage response = await client.PostAsJsonAsync(BaseUrl + "api/pessoas", p);
+
+            return RedirectToAction("pessoas");
+        }
+
+        public IActionResult Alterar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Alterar(Pessoa p, int id)
+        {
+            string BaseUrl = "http://localhost:5043/";
+
+            HttpClient client = new HttpClient();
+
+            HttpResponseMessage response = await client.PutAsJsonAsync(BaseUrl + "api/pessoas/" + id, p);
+
+            return RedirectToAction("pessoas");
+
+        }
+
+        public IActionResult Deletar()
+        {
+            return View();
+        }
+
+        public async Task<ActionResult> Deletar(int id)
+        {
+            string BaseUrl = "http://localhost:5043/";
+
+            HttpClient client = new HttpClient();
+
+            HttpResponseMessage response = await client.DeleteAsync(BaseUrl + "api/pessoas/" + id);
+
+            return RedirectToAction("pessoas");
+
+        }
+
+    }
+}
---
+++ ConsumirApi/Models/Pessoa.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ConsumirApi.Models
+{
+    public class Pessoa
+    {
+
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [Display(Name = "Nome da pessoa: ")]
+        [StringLength(100, MinimumLength = 6)]
+        public string Nome { get; set; }
+
+
+    }
+}
